13e18b8aa221cd5fcc66b235b494fa3144cf738a
Andrew Mehri <90474024+aamehri@users.noreply.github.com>
2023-04-21 19:20:12 -0400

    Add files via upload

---
+++ DataNode.java
@@ -0,0 +1,52 @@
+import javax.xml.crypto.Data;
+
+public class DataNode {
+    //Attributes
+    private int key;
+    private String data;
+    private DataNode left;
+    private DataNode right;
+    //constructor
+    public DataNode(int key, String data)
+    {
+        this.key = key;
+        this.data = data;
+    }
+    //Accessors
+    public String toString()
+    {
+        return "Key: " + getKey() + " Data: " + getData();
+    }
+    public DataNode getLeft(){
+        return left;
+    }
+
+    public DataNode getRight() {
+        return right;
+    }
+
+    public int getKey(){
+        return key;
+    }
+    public String getData(){
+        return data;
+    }
+    //mutators
+    public DataNode setData(int key, String data)
+    {
+        this.key = key;
+        this.data = data;
+        return this;
+    }
+    public DataNode setLeft(DataNode left)
+    {
+        this.left = left;
+        return this;
+    }
+    public DataNode setRight(DataNode right)
+    {
+        this.right = right;
+        return this;
+    }
+
+}
